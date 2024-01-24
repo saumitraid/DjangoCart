2bfac4435b153cdbadf9cd3718edb7be1c84fad5
saumitraid <saumitraid.das@gmail.com>
2024-01-24 15:34:48 +0530

    display product image on admin part

--- ecommerce/cart/admin.py
+++ ecommerce/cart/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Product, CartItem
+from django.utils.html import format_html
 
-admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    def prd_image(self, obj):
+        return format_html('<img src="{}" width="50" height="50"/>'.format(obj.image.url))
+    
+    list_display=('name','description','price','prd_image')
+
 admin.site.register(CartItem)
